a2ba8c307a22c1a6d887d897d7c5c10f2dded858
deus-oc <anishagrawal.2612@gmail.com>
2021-10-07 09:39:23 +0530

    added favourite feature-2

---
+++ src/components/views/MovieDetail/MovieDetail.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import { Row, Typography } from 'antd';
+import { API_URL, API_KEY, IMAGE_BASE_URL, IMAGE_SIZE } from '../../apiConfig'
+import GridCard from '../LandingPage/Section/GridCard';
+import MainImage from '../LandingPage/Section/MainImage';
+import MovieInfo from './Section/MovieInfo';
+import Favourite from './Section/Favourite';
+
+
+const {Title} = Typography;
+
+function MovieDetail(props) {
+    const movieId = props.match.params.movieId
+    const [Movie, setMovie] = useState([])
+    const [Casts, setCasts] = useState([])
+    const [LoadingForMovie, setLoadingForMovie] = useState(true)
+    const [LoadingForCasts, setLoadingForCasts] = useState(true)
+
+    useEffect(() => {
+        let endpointForMovieInfo = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+        fetchDetailInfo(endpointForMovieInfo)
+    }, [])
+
+    const fetchDetailInfo = (endpoint) => {
+
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                setMovie(result)
+                setLoadingForMovie(false)
+
+                let endpointForCasts = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+                fetch(endpointForCasts)
+                    .then(result => result.json())
+                    .then(result => {
+                        setCasts(result.cast)
+                    })
+
+                setLoadingForCasts(false)
+            })
+            .catch(error => console.error('Error:', error)
+            )
+    }
+
+    return (
+        <div>
+            {/* Header */}
+            {!LoadingForMovie ?
+                <MainImage
+                    image={`${IMAGE_BASE_URL}${IMAGE_SIZE}${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+                :
+                <div>loading...</div>
+            }
+
+
+            {/* Body */}
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Favourite movieInfo={Movie} movieId={movieId} userFrom={localStorage.getItem('userId')} />
+                </div>
+
+
+                {/* Movie Info */}
+                {!LoadingForMovie ?
+                    <MovieInfo movie={Movie} />
+                    :
+                    <div>loading...</div>
+                }
+                <br />
+
+                <Title level={2}> Cast </Title>
+                <hr />
+                <br />
+
+                {/* Actors Grid*/}
+                    <Row gutter={[16, 16]}>
+                        {
+                            !LoadingForCasts ? Casts.map((cast, index) => (
+                                cast.profile_path &&
+                                <React.Fragment key={'members' + index}>                                
+                                    <GridCard actor keyVal={'cast'+index} image={cast.profile_path} characterName={cast.characterName} />
+                                </React.Fragment>
+
+                            )) :
+                                <div>loading...</div>
+                        }
+                    </Row>
+                <br />
+            </div>
+
+        </div>
+    )
+}
+
+export default MovieDetail
+
---
+++ src/components/views/MovieDetail/Section/Favourite.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react'
+import { Button } from 'antd';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+
+
+
+function Favourite(props) {
+    const user = useSelector(state => state.user)
+    const variables = {
+        movieId: props.movieId,
+        userFrom: props.userFrom,
+        movieTitle: props.movieInfo.title,
+        moviePost: props.movieInfo.backdrop_path,
+        movieRunTime: props.movieInfo.runtime
+    } 
+    const [FavouriteNumber, setFavouriteNumber] = useState(0)
+    const [Favourited, setFavourited] = useState(false);
+
+    useEffect(() => {
+
+        axios.post('/api/favourite/favouriteNumber', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavouriteNumber(response.data.subscribeNumber)
+                } else {
+                    alert('Failed to get Favourite Number')
+                }
+            })
+
+        axios.post('/api/favourite/favourited', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavourited(response.data.subcribed)
+                } else {
+                    alert('Failed to get Favourite Information')
+                }
+            })
+
+    }, [])
+
+    const onClickFavourite = () => {
+        if (user.userData && !user.userData.isAuth) {
+            return alert('Please Log in first');
+        }
+
+        if (Favourited) {
+            //when we are already subscribed 
+            axios.post('/api/favourite/removeFromFavourite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavouriteNumber(FavouriteNumber - 1)
+                        setFavourited(!Favourited)
+                    } else {
+                        alert('Failed to Remove From Favorite')
+                    }
+                })
+
+        } else {
+            // when we are not subscribed yet
+            axios.post('/api/favourite/addToFavourite', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavouriteNumber(FavouriteNumber + 1)
+                        setFavourited(!Favourited)
+                    } else {
+                        alert('Failed to Add To Favourite')
+                    }
+                })
+        }
+    }
+
+    return (
+        <>
+            <Button onClick={onClickFavourite} > {!Favourited ? "Favourite " : "!Favourite "} {user.userData && user.userData.isAuth ? FavouriteNumber: ''}</Button>
+        </>
+    )
+}
+
+export default Favourite;
